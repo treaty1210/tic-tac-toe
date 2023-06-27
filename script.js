f88fef7099f925695575a0655f6c7ccb2d7efeda
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-06-27 14:51:42 -0700

    Set up functions that allow marking on choice
    Function was created and existing function "render" was adjusted to
    fit in new logic for using buttons to mark a specific spot.

--- script.js
+++ script.js
@@ -4,19 +4,45 @@ let Gameboard = (() => {
     return {board};
 })();
 
-//module for Gamecontroller
-// let Gamecontroller = (() => {
-// })();
+// module for Gamecontroller
+let Gamecontroller = (() => {
+    //add marks to board
+    const placeMarker = document.querySelectorAll(".gameBtn");
+
+    let index = 0;
+    placeMarker.forEach(place => {
+        place.dataset.linkedButton = index;
+        place.addEventListener("click", render);
+
+        //moved render function here and changed it to loop through each node
+        function render() {
+            const gridBoxes = document.querySelectorAll(".gridBox");
+
+            let index = 0;
+            gridBoxes.forEach(gridBox => {
+                gridBox.dataset.linkedButton = index;
+
+                if (gridBox.getAttribute("data-linked-button") == place.getAttribute("data-linked-button")) {
+                    gridBox.textContent = Gameboard.board[Gameboard.board.length - 1];
+                    console.log(place.dataset.linkedButton)
+                    console.log(gridBox.dataset.linkedButton)
+                } 
+                index++
+            })
+        }
+        index++
+    })
+})();
 
 //factory for players
 let createPlayer = (playerName, marker) => {
     return {playerName, marker};
 };
 
-let render = (() => {
-    const gridBoxes = document.querySelectorAll(".gridBox");
-    for (i = 0; i < 9; i++) {
-        gridBoxes[i].textContent = Gameboard.board;
-    }
-    return {};
-})()
+
+
+function box() {
+    placeMarker.addEventListener("click", function() {
+        alert("is this working")
+    })
+}
