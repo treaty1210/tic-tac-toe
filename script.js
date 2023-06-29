03aa2dbd190e11830fa97fbb214a3b1935f4ec80
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-06-29 15:31:38 -0700

    Rewrote and rearranged functions
    Rewrote the gameController to gameBoard.
    gameBoard will now create the array instead of using pre-set html elem.
    This allowed better manipulation within the script to check for
    winners and switching turns.
    
    Rewrote core gameplay in gameBoard relying on click on html divs instead
    of buttons for a cleaner look.
    Since there are no more buttons that disappear on click, created prevent
    event to stop overriding clicks.
    
    Moved player constants into gameController.
    Created a switch turn function and a winCheck function.

--- script.js
+++ script.js
@@ -1,53 +1,112 @@
-//module for Gameboard
-let Gameboard = (() => {
+let displayMsg = document.querySelector(".displayMsg");
+
+
+//module for gameBoard
+let gameBoard = (() => {
+
+    //Create a board array
     let board = [];
+    for (i = 0; i < 9; i++) {
+        board.push("");
+    }
+
+    //query selector for board
+    let gridBox = document.querySelector(".gridBox")
+
+    board.forEach((item, index) => {
+        const box = document.createElement("div");
+        box.className = "box";
+        gridBox.appendChild(box);
+    })
+    
+    Array.from(gridBox.children).forEach((box, index) => {
+        box.addEventListener("click", () => {
+
+            //uses current player's marker
+            box.textContent = gameController.currentPlayer.marker
+            //updates the array value to match marker
+            board[index] = gameController.currentPlayer.marker;
+
+            //prevent overriding of clicks
+            box.style.pointerEvents = "none";
+
+            //after every click, a spot should close
+            gameController.openSpots -= 1;
+
+            //check for winner
+            gameController.winCheck();
+            if (gameController.win == false) {
+                if (gameController.openSpots > 0) {
+                    gameController.switchPlayer();
+                } else if (gameBoard.openSpots == 0) {
+                    console.log("tie")
+                }
+            }
+        })
+    })
     return {board};
 })();
 
-// module for Gamecontroller
-let Gamecontroller = (() => {
-    //add marks to board
-    const placeMarker = document.querySelectorAll(".gameBtn");
-
-    let index = 0;
-    placeMarker.forEach(place => {
-        place.dataset.linkedButton = index;
-        place.addEventListener("click", render);
-
-        //moved render function here and changed it to loop through each node
-        function render() {
-            const gridBoxes = document.querySelectorAll(".gridBox");
-
-            let index = 0;
-            gridBoxes.forEach(gridBox => {
-                gridBox.dataset.linkedButton = index;
-
-                if (gridBox.getAttribute("data-linked-button") == place.getAttribute("data-linked-button")) {
-
-                    //adding turn switching between player1 and player 2
-                    if (player1.turn === true) {
-                    gridBox.textContent = player1.marker;
-                    player1.turn = false;
-                    player2.turn = true;
-                    } else if (player2.turn === true) {
-                        gridBox.textContent = player2.marker;
-                        player1.turn = true;
-                        player2.turn = false;
-                    } else {
-                        return;
-                    }
-                } 
-                index++
-            })
-        }
-        index++
-    })
-})();
-
 //factory for players
-let createPlayer = (playerName, marker, turn) => {
-    return {playerName, marker, turn};
+let createPlayer = (playerName, marker) => {
+    return {playerName, marker};
 };
 
-const player1 = createPlayer("player 1", "X", true);
-const player2 = createPlayer("player 2", "O", false);
+
+
+
+// module for gameController
+let gameController = (() => {
+    
+    const player1 = createPlayer("player 1", "X");
+    const player2 = createPlayer("player 2", "O");
+
+    //Switch player turn
+    let currentPlayer = player1;
+
+    function switchPlayer() {
+        if (this.currentPlayer === player1) {
+            this.currentPlayer = player2;
+        } else {
+            this.currentPlayer = player1;
+        }
+    }
+
+    let openSpots = 9;
+    let win = false;
+    //Win condition
+    const winCondition = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    //win check
+    function winCheck() {
+        winCondition.forEach((item, index) => {
+            if (gameBoard.board[item[0]] === this.currentPlayer.marker && gameBoard.board[item[1]] === this.currentPlayer.marker && gameBoard.board[item[2]] === this.currentPlayer.marker) {
+                this.win = true
+                console.log(`${this.currentPlayer.name} winner`)
+                displayMsg.textContent = `${this.currentPlayer.name} is the winner!`;
+            }
+        })
+    }
+        return {
+            win,
+            currentPlayer,
+            switchPlayer,
+            openSpots,
+            winCheck,
+        }
+})();
+
+
+
+    
+
+
